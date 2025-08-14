c07749ce99a66ebcf72207766ba1c764e16b5326
MatZar02 <mateusz.zarski@newsecuritysystems.pl>
2025-08-14 02:34:25 +0200

    SD working methods v2

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
-'''
+"""
 @author: mromaszewski@iitis.pl, kfilus@iitis.pl -- OG methods
 @author: mzarski@iitis.pl -- getting this mess to actually work
-'''
+"""
 from .args_manager import get_args, create_dir
 from .print_style import Style
 from .compute_SDs import compute_wn_depths, compute_net_depths
