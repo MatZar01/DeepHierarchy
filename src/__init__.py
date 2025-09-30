ae34c0558513c70f6fc2c51e02834384eca1b7c3
Mateusz Żarski <zski.mateusz@gmail.com>
2025-09-30 14:15:33 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 """
 @author: mromaszewski@iitis.pl, kfilus@iitis.pl -- OG methods
-@author: mzarski@iitis.pl -- getting this mess to actually work
+@author: mzarski@iitis.pl -- getting this to work
 """
 from .args_manager import get_args, create_dir
 from .print_style import Style
